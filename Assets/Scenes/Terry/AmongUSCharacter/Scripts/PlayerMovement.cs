306123c8b47f071f6148c2e7cc5e6b6fa52abc83
TerryTsePakLueng <terrytse22@gmail.com>
2020-11-30 19:53:52 -0500

    Added some stuffs to my scene
    teryr scene populated

---
+++ Assets/Scenes/Terry/AmongUSCharacter/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("jump");
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        float speed = Vector3.Distance(lastPosition, transform.position) / Time.deltaTime;
+        animator.SetFloat("speed", speed);
+        lastPosition = this.transform.position;
+    }
+}
