fce72dc51769d2ea67eed8fb186554144b0934de
TerryTsePakLueng <terrytse22@gmail.com>
2020-11-30 23:14:21 -0500

    PlayerController and Animations
    pushing animations, controllers and stuffs

--- Assets/Scenes/Terry/AmongUSCharacter/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Terry/AmongUSCharacter/Scripts/PlayerMovement.cs
@@ -9,14 +9,16 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
+    private float jumpHeight = 0.35f;
     private float gravityValue = -9.81f;
 
     private Vector3 lastPosition;
 
-    private void Start()
+    public static PlayerMovement instance;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
     void Update()
@@ -35,9 +37,9 @@ public class PlayerMovement : MonoBehaviour
             gameObject.transform.forward = move;
         }
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKey(KeyCode.Space) && groundedPlayer)
         {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
             animator.SetTrigger("jump");
         }
 
@@ -47,5 +49,10 @@ public class PlayerMovement : MonoBehaviour
         float speed = Vector3.Distance(lastPosition, transform.position) / Time.deltaTime;
         animator.SetFloat("speed", speed);
         lastPosition = this.transform.position;
+    }
+
+    public void FixedUpdate()
+    {
+        //GameObject.Find("other_object_name").GetComponent(B).enabled = false; (to disable player movement)
     }
 }
