399cf206a185c3168107c877b35e790dd06d1456
FastLite <Volodin_vlad2002@mail.ru>
2020-12-02 19:26:38 +0300

    Scene manager and test scenes for it are added. Scene manager is an instance to it will be able to be accessed on any scene if manager exists

---
+++ Assets/Scenes/Vlad/ButtonScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScene : MonoBehaviour
+{
+    public void LoadScene(int par)
+    {
+        Debug.Log("2");
+        SceneManager_vlad.instance.StartNextLevel(par);
+
+    }
+    
+}
---
+++ Assets/Scenes/Vlad/SceneManager_vlad.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager_vlad : MonoBehaviour
+{
+    public int currentScene = 0;
+    public static SceneManager_vlad instance;
+
+    private void Awake()
+    {
+        
+        DontDestroyOnLoad(this);
+        if (instance== null)
+        {
+            instance = this;
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+        
+
+    }
+
+    public void StartNextLevel(int nextScene)
+    {
+        
+       // UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(currentScene);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+
+        currentScene = nextScene;
+        if (currentScene != 0)
+        {
+            PlayerPrefs.SetInt("LastScenePlayed", currentScene);
+        }
+    }
+
+    public void ContinueGame()
+    {
+        StartNextLevel(PlayerPrefs.GetInt("LastScenePlayed"));
+    }
+
+    public void Restart()
+    {
+        StartNextLevel(currentScene);
+    }
+    
+    
+
+}
